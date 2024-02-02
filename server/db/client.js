62ed28bee68c5b5fc65368cd913c4e4a8ac9fb52
Dylan Wicker <dylanwicker@Dylans-MacBook-Air.local>
2024-02-02 11:56:44 -0500

    changed db

--- server/db/client.js
+++ server/db/client.js
@@ -1,6 +1,6 @@
 const { Client } = require('pg');
 
 const dbName = "bleaf"
-const client = new Client(`postgres://localhost:5431/bleaf`)
+const client = new Client(`postgres://localhost:5431/${dbName}`)
 
 module.exports = client;
