b718e2b40d5f582d59818102409a071bbda24260
Estela <estela.sharp@yahoo.com>
2024-02-07 15:57:36 -0500

    added navigations

--- client/App.js
+++ client/App.js
@@ -1,5 +1,10 @@
 import AppNavigation from "./navigation/appNavigation.js";
+import React from "react";
+import MainContainer from "./navigation/MainContainer.js";
 
 export default function App() {
-  return <AppNavigation />;
+  return (
+    <AppNavigation />
+    
+  )
 }
