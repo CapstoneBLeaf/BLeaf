df7a9238f4eb225d323c01f02ce3b3b41a46ce8b
tanvi4248 <tanvi@GWTN141-10>
2024-02-12 16:59:11 -0600

    remove maincontainer

--- client/App.js
+++ client/App.js
@@ -2,7 +2,6 @@ import store from "./app/store.js";
 import AppNavigation from "./navigation/appNavigation.js"
 import { Provider } from "react-redux";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import MainContainer from "./navigation/MainContainer.js";
 export default function App() {
   return (
     <Provider store={store}>
