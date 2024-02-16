0321a32d30dd9bc252e2445239c8daef052f01a4
Dylan Wicker <dylanwicker27@gmail.com>
2024-02-16 11:24:48 -0500

    added user plant

---
+++ client/plants/plants.js
@@ -0,0 +1,107 @@
+import img1 from './plantImg1.png'
+import img2 from './plantImg2.png'
+import img3 from './plantImg3.png'
+import img4 from './plantImg4.png'
+import img5 from './plantImg5.png'
+import img6 from './plantImg6.png'
+import img7 from './plantImg7.png'
+import img8 from './plantImg8.png'
+import img9 from './plantImg9.png'
+import img10 from './plantImg10.png'
+import img11 from './plantImg11.png'
+import img12 from './plantImg12.png'
+import img13 from './plantImg13.png'
+import img14 from './plantImg14.png'
+import img15 from './plantImg15.png'
+import img16 from './plantImg16.png'
+import img17 from './plantImg17.png'
+import img18 from './plantImg18.png'
+import img19 from './plantImg19.png'
+import img20 from './plantImg20.png'
+import img21 from './plantImg21.png'
+import img22 from './plantImg22.png'
+import img23 from './plantImg23.png'
+import img24 from './plantImg24.png'
+import img25 from './plantImg25.png'
+import img26 from './plantImg26.png'
+import img27 from './plantImg27.png'
+import img28 from './plantImg28.png'
+import img29 from './plantImg29.png'
+import img30 from './plantImg30.png'
+import img31 from './plantImg31.png'
+import img32 from './plantImg32.png'
+import img33 from './plantImg33.png'
+import img34 from './plantImg34.png'
+import img35 from './plantImg35.png'
+import img36 from './plantImg36.png'
+import img37 from './plantImg37.png'
+import img38 from './plantImg38.png'
+import img39 from './plantImg39.png'
+import img40 from './plantImg40.png'
+import img41 from './plantImg41.png'
+import img42 from './plantImg42.png'
+import img43 from './plantImg43.png'
+import img44 from './plantImg44.png'
+import img45 from './plantImg45.png'
+import img46 from './plantImg46.png'
+import img47 from './plantImg47.png'
+import img48 from './plantImg48.png'
+import img49 from './plantImg49.png'
+import img50 from './plantImg50.png'
+import img51 from './plantImg51.png'
+
+const img_arr = [
+    img1,
+    img2,
+    img3,
+    img4,
+    img5,
+    img6,
+    img7,
+    img8,
+    img9,
+    img10,
+    img11,
+    img12,
+    img13,
+    img14,
+    img15,
+    img16,
+    img17,
+    img18,
+    img19,
+    img20,
+    img21,
+    img22,
+    img23,
+    img24,
+    img25,
+    img26,
+    img27,
+    img28,
+    img29,
+    img30,
+    img31,
+    img32,
+    img33,
+    img34,
+    img35,
+    img36,
+    img37,
+    img38,
+    img39,
+    img40,
+    img41,
+    img42,
+    img43,
+    img44,
+    img45,
+    img46,
+    img47,
+    img48,
+    img49,
+    img50,
+    img51
+    ]
+    
+export default img_arr
