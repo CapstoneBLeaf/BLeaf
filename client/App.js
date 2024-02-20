29d38e225e48a8d88aebf424c55bfa560e1807cd
tanvi4248 <tanvi@GWTN141-10>
2024-02-20 17:13:55 -0600

    update coode

--- client/App.js
+++ client/App.js
@@ -1,15 +1,15 @@
 import store from "./app/store.js";
-import AppNavigation from "./navigation/appNavigation.js"
+import AppNavigation from "./navigation/appNavigation.js";
 import { Provider } from "react-redux";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import React from 'react';
+import React from "react";
 
 export default function App() {
   return (
     <Provider store={store}>
       <SafeAreaProvider>
-        <AppNavigation/>
+        <AppNavigation />
       </SafeAreaProvider>
     </Provider>
   );
-}  
+}
