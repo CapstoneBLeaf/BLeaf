6ccb8f67a1567e33510775082523733e690661cb
tanvi4248 <tanvi@GWTN141-10>
2024-02-23 14:39:38 -0600

    spacing correction

--- client/screens/ActivityScreen.js
+++ client/screens/ActivityScreen.js
@@ -75,7 +75,7 @@ export default function ActivityScreen() {
             ) : (
               <SafeAreaView>
                 <Text style={styles.error}>
-                  No checkin habits for selected date.Go to the habits page to
+                  No checkin habits for selected date. Go to the habits page to
                   get started!
                 </Text>
               </SafeAreaView>
